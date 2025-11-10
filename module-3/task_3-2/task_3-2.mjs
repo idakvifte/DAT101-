93c2720984ef1471ca3649224d3fadc645fe8dea
ida kvifte <idakvifte89@gmail.com>
2025-11-10 14:50:00 +0100

    3.2 ferdig

--- module-3/task_3-2/task_3-2.mjs
+++ module-3/task_3-2/task_3-2.mjs
@@ -2,37 +2,135 @@
 import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+//Linje 1: 1-10
+let tekst = "";
+for (let i = 1; i <= 10; i++) {
+    tekst += i + (i < 10? " ":" ");
+}
+printOut(tekst);
+printOut (newLine);
+
+//Linje 2: 10-1
+tekst = "";
+for (let i =10; i >= 1; i--){
+    tekst += i + (i < 1 ? " ": " ");
+}
+printOut (tekst);
+printOut (newLine); 
+
+
+
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+// Tallet vi tenker på
+let riktigTall=45;
+
+// Datamaskinen skal gjette til den treffer riktig
+let gjett = 0;
+
+// Så lenge tallet ikke er riktig, fortsett å gjette 
+while (gjett!== riktigTall) {
+    gjett = Math.floor(Math.random()* 60)+1;
+}
+// Når løkka er ferdig, skriv resultatet
+printOut("Datamaskinen gjettet riktig tall: " + gjett);
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+// Tallet vi tenker på
+let riktigTall2 = 450000;
+
+
+// Teller hvor mange forsøk
+let forsok = 0;
+
+// Starter klokke
+let start = Date.now();
+
+let gjett2 = 0;
+
+// Gjetter til det treffer
+while (gjett2 !== riktigTall2) {
+    gjett2 = Math.floor(Math.random() * 1000000) + 1;
+    forsok++;
+}
+// Stopper klokke
+let slutt = Date.now();
+
+// Regner ut tid brukt
+let tid = slutt - start;
+ 
+// Skriver ut resultatet
+printOut("Datamaskinen fant tallet: " + gjett2);
+printOut("Antall forsøk: " + forsok);
+printOut("Tid brukt (millisekunder): " + tid);
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+for (let tall = 2; tall < 200; tall++) {
+    let erPrim = true;
+
+    for (let dele = 2; dele < tall; dele++) {
+        if (tall % dele === 0) {
+            erPrim= false;
+        break;
+    }
+}
+if (erPrim) {
+    printOut (tall);
+}
+ }
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+// lager 7 rader
+for (let rad = 1; rad <= 7; rad ++) {
+    let linje = ""; // tom linje som skal fylles
+    
+    // lager 9 kolonner på hver rad
+    for (let kol = 1; kol <= 9; kol ++) {
+        linje += "K" + kol + "R" + rad + " "; // legger til kolonne og rad 
+    }
+    // Skriver ut hele raden etter kolonnene er ferdige
+    printOut (linje);
+}
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+const task6Poeng = Math.floor(Math.random() * 236) +1; 
+const task6Prosent = task6Poeng / 236 * 100; 
+let karakter; 
+if (task6Prosent < 40) {
+    karakter = "F"
+     } else if (task6Prosent <52) {
+    karakter = "E"
+     } else if (task6Prosent <62) {
+    karaktre = "D"
+     } else if (task6Prosent <76) {
+    karakter = "C"
+     } else if (task6Prosent <88) { 
+    karakter = "B"
+     } else { 
+    karakter = "A"    
+    }
+printOut ("Studenten fikk " + task6Prosent.toFixed (0) + "%, som tilsvarer " + task6Poeng.toString() + "poeng." );
+printOut ("Dette tilsvarer karakteren " + karakter.toString ()); 
+    
+
+       
+    
+
+    
+
+    
+    
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+//hopper over denne siden den er så krevende. jeg må fokusere på det jeg kan.
 printOut("Replace this with you answer!");
 printOut(newLine);
 
--- module-4/task_4-2/task_4-2.mjs
+++ module-4/task_4-2/task_4-2.mjs
@@ -3,31 +3,85 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+const part1Array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+
+printOut ("Lengde = "+ part1Array.length + ", Verdi = "+ part1Array[part1Array.length - 1]);
+let partText = "";
+for(let i = 0; i < part1Array.length;i++){
+    const value = part1Array [i]; // -> Every index of part1Array 
+    if (i === part1Array.length - 1) {
+        partText += value.toString () + ".";
+    } else {  
+    partText += value.toString() + ",";
+} 
+ }
+printOut(partText); 
+
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const part2Text = part1Array.join(",");
+printOut (part2Text);
+
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
-
+const part3Greeting = "Hello there, how are you?";
+const greetingArray = part3Greeting.split (" ");
+let part3Text = ""; 
+for (let i = 0; i < greetingArray.length; i++){
+  const word = greetingArray [i];
+  // Here you can remove any unwanted characters e.: , ? et. 
+  part3Text += "Index: " + i.toString() + " = " + word + newLine; 
+}
+printOut (part3Text);
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+const girls = ["Anne", "Inger", "Kari", "Marit", "Inger", "Liv", "Eva", "Berit", "Astrid", "Bjørg", "Hilde", "Anna", "Solveig", "Marianne", "Randi", "Ida", "Nina", "Maria", "Elisabeth", "Kristin"]
+
+function removeNameFromArray(aArray, aName) {
+    let deleteIndex = -1;
+    for (let i = 0; i < aArray.length; i++) {
+        const name = aArray [i];
+        if (name === aName) {
+            // Her kan vi slette elementet for eksempel "Hilde"
+            // Dette gjør vi ikke her! Her løper vi igjen, og må slette senere!
+            // Vi må lagre indeksen i en variabel. 
+            deleteIndex = i; 
+        }
+    } 
+    // Teste om jeg kan slette
+    if(deleteIndex >= 0) {
+        printOut (aName + "is found, and deleted.");
+            aArray.splice(deleteIndex, 1)
+    } else { 
+            printOut (aName + " is not found!");
+    } 
+}   
+removeNameFromArray(girls, "Hilde");
+printOut (girls);
+printOut (newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+// https://www.w3schools.com/jsref/jsref_join.asp, https://www.w3schools.com/jsref/jsref_concat_array.asp
+
+const boys= ["Jakob", "Lucas", "Emil", "Oskar", "Oliver", "William", "Filip", "Noah",
+"Elias", "Isak", "Henrik", "Aksel", "Kasper", "Mathias", "Jonas", "Tobias", "Liam", "Håkon", "Theodor","Magnus"];
+
+const allNames= girls.concat(boys); 
+
+printOut (allNames.join (" , ")); 
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+
+function Tbook (aTitle, aAuthor, aISBN)  {
+
+    
+}
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
